78bc0f35cf7f84605b32bf2fac561c00f2844931
marwane ahansal <ahansalmarwane@gmail.com>
2021-07-23 20:15:20 +0100

    fix dashboard line chart axpect ratio

--- frontend/eventook-frontend/src/components/BookingsChart.vue
+++ frontend/eventook-frontend/src/components/BookingsChart.vue
@@ -40,7 +40,6 @@ export default {
         },
         options: {
           responsive: true,
-          maintainAspectRatio: false,
           lineTension: 1,
           scales: {
             yAxes: [
