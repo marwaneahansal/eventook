01737aa7356bb638b37f8cf98cfb6bb58f8933b3
marwane ahansal <ahansalmarwane@gmail.com>
2021-07-24 00:38:21 +0100

    edit api url

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = 'https://eventook-api.herokuapp.com/api/';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 axios.defaults.withCredentials = true;
 
