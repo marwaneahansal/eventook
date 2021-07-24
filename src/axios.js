30b571c841825fc5dc307c3c770745ae27fc2628
marwane ahansal <ahansalmarwane@gmail.com>
2021-07-24 19:21:10 +0100

    production proxy

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = '/api/';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:8000/api';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 axios.defaults.withCredentials = true;
 
