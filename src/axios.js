7d1d420fc87746e4ae3ebc27bf09fdb7d55f4f78
marwane ahansal <ahansalmarwane@gmail.com>
2021-07-24 14:21:31 +0100

    access control

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = `${process.env.VUE_APP_BACKEND_URL}/api/`;
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://eventook-api.herokuapp.com';
 axios.defaults.withCredentials = true;
 
 export default axios;
