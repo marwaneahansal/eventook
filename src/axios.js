4539603579742f4ac359182a52a4c7b14224416c
marwane ahansal <ahansalmarwane@gmail.com>
2021-07-24 16:50:15 +0100

    netlify proxy

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = `${process.env.VUE_APP_BACKEND_URL}/api/`;
+axios.defaults.baseURL = '/api/';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 axios.defaults.withCredentials = true;
 
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,7 +3,7 @@
     <div class="event-wrapper_container is-flex is-flex-direction-column is-flex-grow-1" @click="$router.push({ name: 'EventDetails', params: { id: event.uid }})">
       <div class="event-header">
         <b-image
-          :src="`${backendUrl}/api/events/images/${event.uid}`"
+          :src="`/api/events/images/${event.uid}`"
           :src-fallback="require('@/assets/images/event02.jpg')"
           :alt="event.title">
         </b-image>
